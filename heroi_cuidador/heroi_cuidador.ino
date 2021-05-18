acbd50aeb2b7134d707004cffbb325143cfcc80c
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-05-17 21:58:36 -0300

    Adicionando implementação da FFT para o EMG

--- heroi_cuidador/heroi_cuidador.ino
+++ heroi_cuidador/heroi_cuidador.ino
@@ -5,6 +5,31 @@
 BluetoothSerial SerialBT;
 MPU6050 mpu6050(Wire);
 
+//---------------------------------------------------------------------------//
+//Example data:
+int data_emg[64] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                   };
+
+byte sine_data [91] =
+{
+  0,
+  4,    9,    13,   18,   22,   27,   31,   35,   40,   44,
+  49,   53,   57,   62,   66,   70,   75,   79,   83,   87,
+  91,   96,   100,  104,  108,  112,  116,  120,  124,  127,
+  131,  135,  139,  143,  146,  150,  153,  157,  160,  164,
+  167,  171,  174,  177,  180,  183,  186,  189,  192,  195,
+  198,  201,  204,  206,  209,  211,  214,  216,  219,  221,
+  223,  225,  227,  229,  231,  233,  235,  236,  238,  240,
+  241,  243,  244,  245,  246,  247,  248,  249,  250,  251,
+  252,  253,  253,  254,  254,  254,  255,  255,  255,  255
+};
+
+float f_peaks[5]; // top 5 frequencies peaks in descending order
+//---------------------------------------------------------------------------//
+
 bool musculo_relaxado;
 bool postura_ereta;
 
@@ -19,10 +44,6 @@ bool aux2;
 bool calibracao = false;
 bool operacao = false;
 
-int valor_emg_atual;
-int media_emg;
-int j;
-
 float angulo_x;
 float angulo_y;
 float angulo_z;
@@ -34,8 +55,6 @@ int valor_calibracao;
 boolean se_inclinou;
 
 //Constantes
-//const int  TETO_VOLTAGEM_REPOUSO = 600;
-//const int  PISO_VOLTAGEM_FADIGA = 800;
 
 const int CONTADOR_EMG = 30;
 const int FAIXA_EMG_ATIVACAO = 800;
@@ -63,7 +82,7 @@ void setup() {
   Wire.begin();
   mpu6050.begin();
   mpu6050.setGyroOffsets(-1.20, -4.25, 1.53);
-  
+
   valor_calibracao = 0;
   musculo_relaxado = true;
   postura_ereta = true;
@@ -102,16 +121,18 @@ void loop() {
 }
 
 void identificaFadigaMuscular() {
-  if (media_emg == CONTADOR_EMG) {
+  int amplitude_emg = (int) FFT(data_emg, 64, 50);
+
+  if (amplitude_emg > FAIXA_EMG_ATIVACAO) {
     musculo_relaxado = false;
     enviar_bluetooth("{MSGComeçou Fadiga Muscular}");
   }
 }
 
-//Musculo em fadiga
 bool checaTerminouFadigaMuscular() {
-  //Fadiga terminou
-  if (media_emg == 0) {
+  int amplitude_emg = (int) FFT(data_emg, 64, 50);
+
+  if (amplitude_emg < FAIXA_EMG_ATIVACAO) {
     enviar_bluetooth("{MSGTerminou Fadiga Muscular}");
     musculo_relaxado_2 = true;
     return true;
@@ -125,8 +146,6 @@ void resetaSistemaEMG() {
   //Desliga motor
   digitalWrite(PINO_MOTOR_ESQUERDO, LOW);
   enviar_bluetooth("{MSGTerminou Fadiga Muscular}");
-
-  media_emg = 0;
 
   musculo_relaxado_2 = false;
   musculo_relaxado = true;
@@ -182,19 +201,13 @@ void leitura_mpu() {
 void leitura_emg() {
 
   //leitura do valor do sensor EMG
-  valor_emg_atual = analogRead(PINO_EMG);
+  int valor_emg_atual = analogRead(PINO_EMG);
 
-  if (valor_emg_atual > FAIXA_EMG_ATIVACAO) {
-    if (media_emg < CONTADOR_EMG ) {
-      media_emg = media_emg + 1;
-    }
+  for (int i=64-1; i>0; i--) {  // delocamento do vetor de dados EMG
+    data_emg[i] = data_emg[i - 1];
   }
 
-  if (valor_emg_atual < FAIXA_EMG_ATIVACAO) {
-    if (media_emg > 0 ) {
-      media_emg = media_emg - 1;
-    }
-  }
+  data_emg[0] = valor_emg_atual;  // insere o valor atual no vetor de dados EMG
 }
 
 bool modo_operacao() {
@@ -357,3 +370,175 @@ void enviar_bluetooth(String mensagem) {
 
   SerialBT.write((uint8_t*)&mensagem_bluetooth, tamanho);
 }
+
+//-----------------------------FFT Function----------------------------------------------//
+
+float FFT(int in[], int N, float Frequency)
+{
+  unsigned int data[13] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
+  int a, c1, f, o, x;
+  a = N;
+
+  for (int i = 0; i < 12; i++)          //calculating the levels
+  {
+    if (data[i] <= a) {
+      o = i;
+    }
+  }
+
+  int in_ps[data[o]] = {};   //input for sequencing
+  float out_r[data[o]] = {}; //real part of transform
+  float out_im[data[o]] = {}; //imaginory part of transform
+
+  x = 0;
+  for (int b = 0; b < o; b++)              // bit reversal
+  {
+    c1 = data[b];
+    f = data[o] / (c1 + c1);
+    for (int j = 0; j < c1; j++)
+    {
+      x = x + 1;
+      in_ps[x] = in_ps[j] + f;
+    }
+  }
+
+
+  for (int i = 0; i < data[o]; i++)     // update input array as per bit reverse order
+  {
+    if (in_ps[i] < a)
+    {
+      out_r[i] = in[in_ps[i]];
+    }
+    if (in_ps[i] > a)
+    {
+      out_r[i] = in[in_ps[i] - a];
+    }
+  }
+
+
+  int i10, i11, n1;
+  float e, c, s, tr, ti;
+
+  for (int i = 0; i < o; i++)                             //fft
+  {
+    i10 = data[i];            // overall values of sine/cosine  :
+    i11 = data[o] / data[i + 1]; // loop with similar sine cosine:
+    e = 360 / data[i + 1];
+    e = 0 - e;
+    n1 = 0;
+
+    for (int j = 0; j < i10; j++)
+    {
+      c = cosine(e * j);
+      s = sine(e * j);
+      n1 = j;
+
+      for (int k = 0; k < i11; k++)
+      {
+        tr = c * out_r[i10 + n1] - s * out_im[i10 + n1];
+        ti = s * out_r[i10 + n1] + c * out_im[i10 + n1];
+
+        out_r[n1 + i10] = out_r[n1] - tr;
+        out_r[n1] = out_r[n1] + tr;
+
+        out_im[n1 + i10] = out_im[n1] - ti;
+        out_im[n1] = out_im[n1] + ti;
+
+        n1 = n1 + i10 + i10;
+      }
+    }
+  }
+
+  //---> here onward out_r contains amplitude and our_in conntains frequency (Hz)
+  for (int i = 0; i < data[o - 1]; i++)      // getting amplitude from compex number
+  {
+    out_r[i] = sqrt(out_r[i] * out_r[i] + out_im[i] * out_im[i]); // to  increase the speed delete sqrt
+    out_im[i] = i * Frequency / N;
+  }
+
+  x = 0;     // peak detection
+  for (int i = 1; i < data[o - 1] - 1; i++)
+  {
+    if (out_r[i] > out_r[i - 1] && out_r[i] > out_r[i + 1])
+    { in_ps[x] = i;  //in_ps array used for storage of peak number
+      x = x + 1;
+    }
+  }
+
+
+  s = 0;
+  c = 0;
+  for (int i = 0; i < x; i++)      // re arraange as per magnitude
+  {
+    for (int j = c; j < x; j++)
+    {
+      if (out_r[in_ps[i]] < out_r[in_ps[j]])
+      { s = in_ps[i];
+        in_ps[i] = in_ps[j];
+        in_ps[j] = s;
+      }
+    }
+    c = c + 1;
+  }
+
+
+  for (int i = 0; i < 5; i++) // updating f_peak array (global variable)with descending order
+  {
+    f_peaks[i] = out_im[in_ps[i]];
+  }
+
+  return out_r[0] / N;
+}
+
+
+float sine(int i)
+{
+  int j = i;
+  float out;
+  while (j < 0) {
+    j = j + 360;
+  }
+  while (j > 360) {
+    j = j - 360;
+  }
+  if (j > -1   && j < 91) {
+    out = sine_data[j];
+  }
+  else if (j > 90  && j < 181) {
+    out = sine_data[180 - j];
+  }
+  else if (j > 180 && j < 271) {
+    out = -sine_data[j - 180];
+  }
+  else if (j > 270 && j < 361) {
+    out = -sine_data[360 - j];
+  }
+  return (out / 255);
+}
+
+float cosine(int i)
+{
+  int j = i;
+  float out;
+  while (j < 0) {
+    j = j + 360;
+  }
+  while (j > 360) {
+    j = j - 360;
+  }
+  if (j > -1   && j < 91) {
+    out = sine_data[90 - j];
+  }
+  else if (j > 90  && j < 181) {
+    out = -sine_data[j - 90];
+  }
+  else if (j > 180 && j < 271) {
+    out = -sine_data[270 - j];
+  }
+  else if (j > 270 && j < 361) {
+    out = sine_data[j - 270];
+  }
+  return (out / 255);
+}
+
+//------------------------------------------------------------------------------------//
